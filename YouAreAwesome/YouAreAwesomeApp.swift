3013567e95b2927d6a160284e082c16efa4e3f8c
gallaugher <john.gallaugher@bc.edu>
2025-08-21 12:32:56 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,15 @@
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//  Created by John Gallaugher on 8/21/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
